1183e4ac7cb3490b80c8531c179f6cf6ee3aec48
Monayem Islam <monayem.dev@gmail.com>
2022-06-07 18:01:29 +0600

    Chart Initialized

---
+++ assets/js/charts/parking-space-type-chart.js
@@ -0,0 +1,60 @@
+
+//setup block
+const parkingSpaceTypeBarChartData = {
+    labels: ['Car', 'Micro', 'Bike', 'Cavard Van'],
+        datasets: [{
+            label: 'Types of User',
+            data: [121, 320,56,300],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(252, 188, 60,.2)',
+            ],
+            hoverOffset:6,
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+}
+//options
+
+const parkingSpaceTypeBarChartOptions = {
+    plugins: {
+        legend:{
+            display:true,
+            position: 'bottom'
+        }
+    },
+    scales: {
+        y: {
+          beginAtZero: true,
+          max: 400,
+          min: 0,
+          ticks: {
+                // callback: function(value, index, values) {
+                //     return value + ' M';
+                // },
+                // stepSize: 5
+            }
+        }
+      }
+}
+
+//config block
+const parkingSpaceTypeBarChartConfig= {
+    type: 'bar',
+    data: parkingSpaceTypeBarChartData,
+    options: parkingSpaceTypeBarChartOptions
+}
+//render block
+const parkingSpaceTypePieChart = new Chart(
+    document.getElementById('parkingSpaceTypePieChart'),parkingSpaceTypeBarChartConfig
+);
+
+
+ 
---
+++ assets/js/charts/user-type-pie-chart.js
@@ -0,0 +1,42 @@
+
+//setup block
+const userTypePieChartData = {
+    labels: ['Providers', 'Finders'],
+        datasets: [{
+            label: 'Types of User',
+            data: [12, 19],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+            ],
+            hoverOffset:6,
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+            ],
+            borderWidth: 1
+        }]
+}
+//options
+
+const userTypePieCharOptions = {
+    plugins: {
+        legend:{
+            display:true,
+            position: 'bottom'
+        }
+    }
+}
+
+//config block
+const userTypePieChartConfig= {
+    type: 'pie',
+    data: userTypePieChartData,
+    options: userTypePieCharOptions
+}
+//render block
+const userTypePieChartRender = new Chart(
+    document.getElementById('userTypePieChart'),userTypePieChartConfig
+);
+
+
